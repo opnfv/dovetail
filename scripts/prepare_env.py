cb55f6dbfe4867e4d232f51b5a32764f82d2399b
Leo Wang <grakiss.wanglei@huawei.com>
2016-09-17 23:15:18 -0400

    add prototype of dovetail tool
    JIRA: DOVETAIL-12
    
    Change-Id: I1ee120ed8199589e1e7cbce9cbb55036e9e5f7ea
    Signed-off-by: Leo Wang <grakiss.wanglei@huawei.com>

---
+++ scripts/prepare_env.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# grakiss.wanglei@huawei.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+import os
+
+import utils.dovetail_logger as dt_logger
+import utils.dovetail_utils as dt_utils
+
+
+logger = dt_logger.Logger('prepare_env.py').getLogger()
+
+cmd = "sudo apt-get -y install docker.io python-pip"
+dt_utils.exec_cmd(cmd, logger)
+
+cmd = "sudo pip install click pyyaml"
+dt_utils.exec_cmd(cmd, logger)
+
