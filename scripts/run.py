14ee4260b372698596f324874444d74ad178cf5b
xudan <xudan16@huawei.com>
2016-10-08 08:42:56 +0000

    Remove scripts in dovetail and use yarstick scripts to prepare environment
    JIRA: DOVETAIL-20
    
    Change-Id: I46a095d5714245a9399e3a33c72f5f4bc77a18bf
    Signed-off-by: xudan <xudan16@huawei.com>

--- scripts/run.py
+++ scripts/run.py
@@ -31,22 +31,6 @@ def load_scenario(scenario):
 def load_testcase():
     Testcase.load()
 
-def run_functest(testcase, container_id):
-    for cmd in testcase.cmds:
-        Container.exec_cmd(container_id, cmd)
-
-def run_yardstick(testcase, container_id):
-    type = testcase.script_type()
-    Container.copy_file(os.path.join(os.getcwd(), dovetail_config[type]['shell_dir_name']),\
-                        container_id, dovetail_config[type]['shell_dir'])
-    if Container.has_build_images[type] == True:
-        sub_cmd = dovetail_config[type]['testcase']['test_cmd'] % (testcase.script_testcase(), testcase.name())
-    else:
-        Container.has_build_images[type] = True
-        sub_cmd = dovetail_config[type]['testcase']['build_test_cmd'] % (testcase.script_testcase(), testcase.name())
-    Container.exec_cmd(container_id, sub_cmd)
-    time.sleep(5)
-
 def run_test(scenario):
     for testcase_name in scenario['testcase_list']:
         logger.info('>>[testcase]: %s' % (testcase_name))
@@ -74,10 +58,8 @@ def run_test(scenario):
             if not testcase.prepare_cmd():
                 logger.error('failed to prepare testcase:%s' % testcase.name())
             else:
-                if testcase.script_type() == 'functest':
-                    run_functest(testcase, container_id)
-                else:
-                    run_yardstick(testcase, container_id)
+                for cmd in testcase.cmds:
+                    Container.exec_cmd(container_id, cmd)
 
             #testcase.post_condition()
 
