5536c9d9e4b2ee10fb971eef67f57e80d682d2c9
zshi <zshi@redhat.com>
2016-10-14 18:32:23 +0800

    Fix unittest errors in container.py
    JIRA: DOVETAIL-35
    
    Change-Id: I0d869ad8e1d918345a90cefd755a56f414de8411
    Signed-off-by: zshi <zshi@redhat.com>

--- dovetail/container.py
+++ dovetail/container.py
@@ -9,14 +9,15 @@
 
 import utils.dovetail_logger as dt_logger
 import utils.dovetail_utils as dt_utils
-from conf.dovetail_config import *
+from conf.dovetail_config import dovetail_config
 
 logger = dt_logger.Logger('container.py').getLogger()
+
 
 class Container:
 
     container_list = {}
-    has_pull_latest_image = {'yardstick':False, 'functest':False}
+    has_pull_latest_image = {'yardstick': False, 'functest': False}
 
     def __init__(cls):
         pass
@@ -30,40 +31,46 @@ class Container:
 
     @classmethod
     def get_docker_image(cls, type):
-        return '%s:%s' % (dovetail_config[type]['image_name'], dovetail_config[type]['docker_tag'])
+        return '%s:%s' % (dovetail_config[type]['image_name'],
+                          dovetail_config[type]['docker_tag'])
 
     @classmethod
     def create(cls, type):
-        #sshkey="-v /root/.ssh/id_rsa:/root/.ssh/id_rsa "
+        # sshkey="-v /root/.ssh/id_rsa:/root/.ssh/id_rsa "
         docker_image = cls.get_docker_image(type)
         envs = dovetail_config[type]['envs']
         opts = dovetail_config[type]['opts']
         sshkey = ''
-        result_volume = ' -v %s:%s ' % (dovetail_config['result_dir'],dovetail_config[type]['result']['dir'])
-        cmd = 'sudo docker run %s %s %s %s %s /bin/bash' % (opts, envs, sshkey, result_volume, docker_image)
-        dt_utils.exec_cmd(cmd,logger)
-        ret, container_id=dt_utils.exec_cmd("sudo docker ps | grep "+ docker_image + " | awk '{print $1}' | head -1",logger)
+        result_volume = ' -v %s:%s ' % (dovetail_config['result_dir'],
+                                        dovetail_config[type]['result']['dir'])
+        cmd = ('sudo docker run %s %s %s %s %s /bin/bash'
+               % (opts, envs, sshkey, result_volume, docker_image))
+        dt_utils.exec_cmd(cmd, logger)
+        ret, container_id = dt_utils.exec_cmd("sudo docker ps | grep "
+                                              + docker_image +
+                                              " | awk '{print $1}' | head -1",
+                                              logger)
         cls.container_list[type] = container_id
         return container_id
 
     @classmethod
     def pull_image(cls, type):
         docker_image = cls.get_docker_image(type)
-        if cls.has_pull_latest_image[type] == True:
+        if cls.has_pull_latest_image[type] is True:
             logger.debug('%s is already the newest version.' % (docker_image))
         else:
             cmd = 'sudo docker pull %s' % (docker_image)
-            dt_utils.exec_cmd(cmd,logger)
+            dt_utils.exec_cmd(cmd, logger)
             cls.has_pull_latest_image[type] = True
 
     @classmethod
     def clean(cls, container_id):
         cmd1 = 'sudo docker stop %s' % (container_id)
-        dt_utils.exec_cmd(cmd1,logger)
+        dt_utils.exec_cmd(cmd1, logger)
         cmd2 = 'sudo docker rm %s' % (container_id)
-        dt_utils.exec_cmd(cmd2,logger)
+        dt_utils.exec_cmd(cmd2, logger)
 
     @classmethod
     def exec_cmd(cls, container_id, sub_cmd, exit_on_error=False):
         cmd = 'sudo docker exec %s /bin/bash -c "%s"' % (container_id, sub_cmd)
-        dt_utils.exec_cmd(cmd,logger,exit_on_error)
+        dt_utils.exec_cmd(cmd, logger, exit_on_error)
