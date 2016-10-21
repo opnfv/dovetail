beaed7ffcde4939c9aa50e54e9baa14f4d0ba8e5
xudan <xudan16@huawei.com>
2016-10-21 08:03:43 +0000

    dovetail tool: Add cli mechanism for changing envs at run-time
    JIRA: DOVETAIL-23
    
    1. Add file cmd_config.yml for defining arguments and options.
    2. Using python click module to add and parse cmds in cmd_config.yml
    3. Using envs options to update envs in yardstick/functest config file
    
    Change-Id: Ie513c0f0a1ed76f29408419a4bcbbcf21334bb3f
    Signed-off-by: xudan <xudan16@huawei.com>

--- dovetail/conf/dovetail_config.py
+++ dovetail/conf/dovetail_config.py
@@ -9,6 +9,7 @@
 
 import yaml
 import os
+import re
 
 CERT_PATH = './cert/'
 TESTCASE_PATH = './testcase/'
@@ -27,3 +28,38 @@ container_config = {}
 
 container_config['functest'] = dovetail_config['functest']
 container_config['yardstick'] = dovetail_config['yardstick']
+
+
+with open(os.path.join(curr_path, 'cmd_config.yml')) as f:
+    cmd_yml = yaml.safe_load(f)
+    dovetail_config['cli'] = cmd_yml[cmd_yml.keys()[0]]
+
+
+def cmd_name_trans(cmd_name):
+    key = cmd_name.upper()
+    if key == 'SUT_TYPE':
+        key = 'INSTALLER_TYPE'
+    if key == 'SUT_IP':
+        key = 'INSTALLER_IP'
+    return key
+
+
+def update_envs(options):
+    for item in options:
+        if options[item] is not None:
+            key = cmd_name_trans(item)
+            os.environ[key] = options[item]
+            update_config_envs('functest', key)
+            update_config_envs('yardstick', key)
+
+
+def update_config_envs(test_type, key):
+    old_value = re.findall(r'\s+%s=(.*?)(\s+|$)' % key,
+                           dovetail_config[test_type]['envs'])
+    if old_value == []:
+        dovetail_config[test_type]['envs'] += \
+            ' -e ' + key + '=' + os.environ[key]
+    else:
+        dovetail_config[test_type]['envs'] = \
+            dovetail_config[test_type]['envs'].replace(old_value[0][0],
+                                                       os.environ[key])
