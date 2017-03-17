ad6288a5530cc2f4cc4fe794552170f6bdebe640
xudan <xudan16@huawei.com>
2017-03-17 09:23:45 +0000

    dovetail tool: remove installer and CI related variables
    JIRA: DOVETAIL-371
    
    1. remove all the envs in functest_config.yml and yardstick_config.yml
    2. remove all cmds in cmd_config.yml
    3. remove all the envs when run a new functest/yardstick container
    4. add some variables just for functest pushing results to db
    5. get the external network name for yardstick
    6. make --debug/-d just control the log level shown on the screen
    
    Change-Id: I9f941e09d067fc95a14c3c9be1374f41df4a9a16
    Signed-off-by: xudan <xudan16@huawei.com>

--- dovetail/utils/dovetail_config.py
+++ dovetail/utils/dovetail_config.py
@@ -16,12 +16,6 @@ class DovetailConfig(object):
 
     dovetail_config = {}
 
-    CMD_NAME_TRANS = {
-        'SUT_TYPE': 'INSTALLER_TYPE',
-        'SUT_IP': 'INSTALLER_IP',
-        'DEBUG': 'CI_DEBUG',
-    }
-
     @classmethod
     def load_config_files(cls):
         curr_path = os.path.dirname(os.path.abspath(__file__))
@@ -39,23 +33,6 @@ class DovetailConfig(object):
             cmd_yml = yaml.safe_load(f)
             cls.dovetail_config['cli'] = cmd_yml[cmd_yml.keys()[0]]
 
-    @classmethod
-    def cmd_name_trans(cls, cmd_name):
-        key = cmd_name.upper()
-        return cls.CMD_NAME_TRANS.get(key, key)
-
-    # Analyze the kind of the giving path,
-    # return true for env path,
-    # return false for non_env path.
-    @classmethod
-    def is_env_path(cls, path):
-        if len(path) == 2:
-            test_project = cls.dovetail_config['test_project']
-            if path[0] in test_project and path[1] == 'envs':
-                return True
-        else:
-            return False
-
     # update dovetail_config dict with the giving path.
     # if path is in the dovetail_config dict, its value will be replaced.
     # if path is not in the dict, it will be added as a new item of the dict.
@@ -66,29 +43,7 @@ class DovetailConfig(object):
             for item in value['path']:
                 path_list.append([(k.strip()) for k in item.split('/')])
             for path in path_list:
-                if cls.is_env_path(path):
-                    cls.update_envs(key, path, value['value'])
-                else:
-                    cls.update_non_envs(path, value['value'])
-
-    @classmethod
-    def update_envs(cls, key, path, value):
-        key = cls.cmd_name_trans(key)
-        if not value and key in os.environ:
-            value = os.environ[key]
-        if value:
-            cls.update_config_envs(path[0], key, value)
-
-    @classmethod
-    def update_config_envs(cls, validate_type, key, value):
-        envs = cls.dovetail_config[validate_type]['envs']
-        old_value = re.findall(r'\s+%s=(.*?)(\s+|$)' % key, envs)
-        if old_value == []:
-            envs += ' -e ' + key + '=' + value
-        else:
-            envs = envs.replace(old_value[0][0], value)
-        cls.dovetail_config[validate_type]['envs'] = envs
-        return envs
+                cls.update_non_envs(path, value['value'])
 
     @staticmethod
     def set_leaf_dict(dic, path, value):
--- dovetail/utils/dovetail_utils.py
+++ dovetail/utils/dovetail_utils.py
@@ -10,8 +10,12 @@
 #
 
 import sys
+import os
+import re
 import subprocess
 from collections import Mapping, Set, Sequence
+from keystoneauth1 import identity, session
+from neutronclient.v2_0 import client
 
 
 def exec_log(verbose, logger, msg, level, flush=False):
@@ -95,6 +99,40 @@ def get_obj_by_path(obj, dst_path):
         if path == dst_path:
             return obj
 
+
+def source_env(env_file):
+    with open(env_file, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        for match in re.findall(r"export (.*)=(.*)", line):
+            match = (match[0].strip('\"'), match[1].strip('\"'))
+            match = (match[0].strip('\''), match[1].strip('\''))
+            os.environ.update({match[0]:match[1]})
+
+
+def get_openstack_session():
+    auth = identity.Password(auth_url=os.environ.get('OS_AUTH_URL'),
+                             username=os.environ.get('OS_USERNAME'),
+                             password=os.environ.get('OS_PASSWORD'),
+                             tenant_name=os.environ.get('OS_TENANT_NAME'))
+    return session.Session(auth=auth)
+
+
+def get_neutron_client():
+    sess = get_openstack_session()
+    neutron_client = client.Client(session=sess)
+    return neutron_client
+
+
+def get_ext_net_name(env_file):
+    source_env(env_file)
+    neutron_client= get_neutron_client()
+    networks = neutron_client.list_networks()['networks']
+    try:
+        ext_network = next(n['name'] for n in networks if n['router:external'])
+        return ext_network
+    except StopIteration:
+        return None
 
 def show_progress_bar(length):
     max_len = 50
