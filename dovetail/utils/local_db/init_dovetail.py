91086b95c0334e0363c157d85001c6f5082c857f
grakiss <grakiss.wanglei@huawei.com>
2017-08-17 17:14:56 +0800

    add nginx to host static files
    JIRA: DOVETAIL-485
    
    Tornado does not support directory listing, so add nginx to host static files, and support log file browsing
    
    Change-Id: Ic1427ab9c64661b3993ac60943f470b125a0fbe7
    Signed-off-by: grakiss <grakiss.wanglei@huawei.com>

---
+++ dovetail/utils/local_db/init_dovetail.py
@@ -0,0 +1,49 @@
+import requests
+import datetime
+import json
+import yaml
+import sys
+
+base_url = sys.argv[1]
+headers = {'Content-Type': 'application/json'}
+
+
+def create_project():
+
+    name = 'dovetail'
+    s = '2015-10-14 06:56:09'
+    time = datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+
+    data = {
+        'name': name,
+        'creation_date': str(time),
+        'description': ''
+    }
+
+    url = '{}/projects'.format(base_url)
+    requests.post(url, data=json.dumps(data), headers=headers)
+
+
+def dovetail():
+    with open('../../compliance/proposed_tests.yml') as f:
+        data = yaml.safe_load(f)['proposed_tests']['testcases_list']
+
+    url = '{}/projects/dovetail/cases'.format(base_url)
+    for case in data:
+        c = {
+            'ci_loop': 'daily',
+            'description': 'dovetail',
+            'name': case,
+            'project_name': 'dovetail',
+            'trust': 'gold',
+            'url': '',
+            'version': 'master',
+            'domains': 'master',
+            'tags': 'dovetail'
+        }
+        requests.post(url, data=json.dumps(c), headers=headers)
+
+
+if __name__ == '__main__':
+    create_project()
+    dovetail()
