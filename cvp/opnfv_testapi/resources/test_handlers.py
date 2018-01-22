9e0073625bb4ffb21ba5c2e4cb74171bb57edb3f
chenjiankun <chenjiankun1@huawei.com>
2018-01-22 02:11:31 +0000

    Add tooltip for SUT version
    We expect users to fill out the SUT version with a fixed format,
    So we need to add a tooltip for user to know the expected format.
    
    In this patch, I also do:
    1. result table self-adaption.
    2. user can't change SUT version after submiting test.
    
    Change-Id: I5c391592dcbdbd9eb8697e44d7bb5dda41431bd6
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- cvp/opnfv_testapi/resources/test_handlers.py
+++ cvp/opnfv_testapi/resources/test_handlers.py
@@ -196,9 +196,15 @@ class TestsGURHandler(GenericTestHandler):
             return
 
         curr_user = self.get_secure_cookie(auth_const.OPENID)
-        if item in {"shared", "label"}:
+        if item in {"shared", "label", "sut_label"}:
             query['owner'] = curr_user
             db_keys.append('owner')
+
+        if item == 'sut_label':
+            if test['status'] != 'private':
+                msg = 'SUT version cannot be changed after submit.'
+                self.finish_request({'code': 403, 'msg': msg})
+                return
 
         if item == "status":
             if value in {'approved', 'not approved'}:
