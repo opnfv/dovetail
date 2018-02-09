1ce95a51fd751c7afd10690530193d2cf44aaefc
Eddie Arrage <eddie.arrage@huawei.com>
2018-02-09 15:52:21 -0800

    Add stop on failure logic and CLI option
    - Improve test execution control by adding stop on failure flag
    - Modify test check function to return value regardless of whether
    report or local file option is employed
    - Modify run test loop to check for failure after every test case
    if stop flag set
    
    Change-Id: I27a4f3aaaa8b8cb8c52eb3f092f52048980abb44
    Signed-off-by: Eddie Arrage <eddie.arrage@huawei.com>

--- dovetail/run.py
+++ dovetail/run.py
@@ -39,7 +39,7 @@ def load_testcase():
     Testcase.load()
 
 
-def run_test(testsuite, testarea, logger):
+def run_test(testsuite, testarea, logger, kwargs):
     testcase_list = Testcase.get_testcase_list(testsuite, testarea)
     duration = 0
     start_time = time.time()
@@ -61,7 +61,9 @@ def run_test(testsuite, testarea, logger):
         if run_testcase:
             testcase.run()
 
-        check_tc_result(testcase, logger)
+        stop_on_fail = check_tc_result(testcase, logger)
+        if stop_on_fail['criteria'] == "FAIL" and kwargs['stop']:
+            break
 
     end_time = time.time()
     duration = end_time - start_time
@@ -99,8 +101,9 @@ def check_tc_result(testcase, logger):
         else:
             logger.error(
                 "Failed to store results with file {}.".format(result_file))
-        result = Report.get_result(testcase)
-        Report.check_result(testcase, result)
+    result = Report.get_result(testcase)
+    Report.check_result(testcase, result)
+    return result
 
 
 def validate_input(input_dict, check_dict, logger):
@@ -298,7 +301,7 @@ def main(*args, **kwargs):
     if testsuite_validation and testarea_validation:
         testsuite_yaml = load_testsuite(kwargs['testsuite'])
         load_testcase()
-        duration = run_test(testsuite_yaml, testarea, logger)
+        duration = run_test(testsuite_yaml, testarea, logger, kwargs)
         if dt_cfg.dovetail_config['report_dest'] == "file":
             Report.generate(testsuite_yaml, testarea, duration)
         if dt_cfg.dovetail_config['report_dest'].startswith("http"):
