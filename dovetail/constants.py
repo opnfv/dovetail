e9982c0fd2fdfa274b41518124c8746404654ac8
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-03-01 20:03:59 +0800

    leverage data_files to eliminate so many hardcoded abs_path_seekings
    1. mv conf/ userconfig/ compliance/ patch to etc/
    2. after installation etc/ will be put under /etc/dovetail
    3. leveraging /etc/dovetail in coding
    
    Change-Id: Icf9cb00ff7fc7cf35755f60d3e258b23e2c70839
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ dovetail/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# feng.xiaowei@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+import os.path
+
+
+DOVETAIL_CONF_PATH = '/etc/dovetail'
+USERCONF_PATH = os.path.join(DOVETAIL_CONF_PATH, 'userconfig')
+PATCH_PATH = os.path.join(DOVETAIL_CONF_PATH, 'patch')
+CONF_PATH = os.path.join(DOVETAIL_CONF_PATH, 'conf')
+TESTCASE_PATH = os.path.join(DOVETAIL_CONF_PATH, 'testcase')
+COMPLIANCE_PATH = os.path.join(DOVETAIL_CONF_PATH, 'compliance')
