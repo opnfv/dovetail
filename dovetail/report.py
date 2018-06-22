6af7f64e6bacc20e9a95db85379fa1f86bb0fe18
Georg Kunz <georg.kunz@ericsson.com>
2018-06-22 21:37:35 +0200

    Update licence headers to comply with guidelines
    Updating the licence headers to comply with the community guidelines [1]
    and anteater checks.
    
    [1] https://wiki.opnfv.org/display/DEV/Contribution+Guidelines#ContributionGuidelines-License
    
    Change-Id: Idec787e32daf76c0b1bd6108977b78828125da5e
    Signed-off-by: Georg Kunz <georg.kunz@ericsson.com>

--- dovetail/report.py
+++ dovetail/report.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+
 #
-# grakiss.wanglei@huawei.com
+# Copyright (c) 2017 grakiss.wanglei@huawei.com and others.
+#
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 #
+
 from __future__ import division
 
 import json
