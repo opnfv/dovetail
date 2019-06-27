b3843397d0941fbf8700341d6802cb027143161e
xudan <xudan16@huawei.com>
2019-06-27 04:37:55 -0400

    Add Swagger UI for dovetail API
    Change-Id: If8f515b02f0372955739dd580967a3198930e98b
    Signed-off-by: xudan <xudan16@huawei.com>

--- dovetail/api/app/routes.py
+++ dovetail/api/app/routes.py
@@ -1,10 +1,12 @@
 #!flask/bin/python
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 import server
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/v1/scenario/nfvi/testsuites', methods=['GET'])
