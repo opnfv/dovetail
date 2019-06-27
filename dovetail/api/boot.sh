1f36d6247578273197a4ba64b58b02b1ed837171
xudan <xudan16@huawei.com>
2019-06-27 04:37:55 -0400

    Add Swagger UI for dovetail API
    Change-Id: If8f515b02f0372955739dd580967a3198930e98b
    Signed-off-by: xudan <xudan16@huawei.com>

--- dovetail/api/boot.sh
+++ dovetail/api/boot.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+mkdir -p /var/www/html/dovetail-api
+cp -r /home/opnfv/swagger-ui/dist/* /var/www/html/dovetail-api
+cp /home/opnfv/dovetail/dovetail/api/swagger.yaml /var/www/html/dovetail-api
+sed -i 's#url: "https://petstore.swagger.io/v2/swagger.json"#url: "swagger.yaml"#g' /var/www/html/dovetail-api/index.html
+sed -i '/deepLinking: true,/a\        validatorUrl: null,' /var/www/html/dovetail-api/index.html
+
+if [[ -n ${SWAGGER_HOST} ]]; then
+    sed -i "s/host: localhost:8888/host: ${SWAGGER_HOST}/g" /var/www/html/dovetail-api/swagger.yaml
+fi
+
+/etc/init.d/apache2 start
+
 cd $(dirname $(readlink -f $0))
 exec gunicorn -b :5000 --access-logfile - --error-logfile - app.routes:app
